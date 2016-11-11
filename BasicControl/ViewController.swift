94a96f9d9b594b9ab5b14be63b846bbfcf5a76c1
teacher <teacher@teacher-no-imac-10.local>
2016-11-11 15:49:33 +0900

    Initial Commit

---
+++ BasicControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicControl
+//
+//  Created by teacher on 2016/11/11.
+//  Copyright © 2016年 teacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
