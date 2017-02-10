4ce091527e2eb2c79c60733a44ee610863ab407c
j13012 <j13012@sangi.jp>
2017-02-10 13:30:19 +0900

    commit BasicControl
    Completed

--- BasicControl/ViewController.swift
+++ BasicControl/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //サンプル用変数
+        let scoreResult:[Int] = [75, 40, 45, 80, 90]
+        var teamScore:Int = 0
+        
+        //サンプル用基本制御
+         for score in scoreResult{
+            if score > 50{
+                teamScore += 3
+            }else{
+                teamScore += 1
+            }
+         }
+         
+         //結果表示用ラベル
+         label.text = "チームの点数は" + String(teamScore)
+         label.textAlignment = NSTextAlignment.center
+        label.font = UIFont(name: "Meyrio", size: 20)
+
+         //ビューに追加
+         self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
